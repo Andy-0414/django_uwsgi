1c15ce4880ca46ba1f031374b1823a3319298a76
Andy0414 <pjh8667@naver.com>
2019-07-29 17:03:59 +0900

    CREATE rp

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pjh86\desktop\earlysloth\django_uwsgi\.venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
