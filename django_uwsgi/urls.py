44a120e00b3913f9a55a647bec3ca5ab0c63b848
Andy0414 <pjh8667@naver.com>
2019-07-30 10:08:32 +0900

    UPDATE auto

--- django_uwsgi/urls.py
+++ django_uwsgi/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 
 urlpatterns = [
     url('admin/', admin.site.urls),
