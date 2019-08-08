fcdb3f73f54d9e302bbc7a44f8d8b864ecc35d32
Andy0414 <pjh8667@naver.com>
2019-08-08 15:56:55 +0900

    UPDATE admin page

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Post,People
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(People)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'imgName', 'title','link']
+    list_display_links = ['id', 'imgName']
+
+@admin.register(People)
+class PeopleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'imgName', 'name','subname','title']
+    list_display_links = ['id', 'imgName','name']
